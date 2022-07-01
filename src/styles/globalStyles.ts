27154b86c508e0adad6698abdba6e56e71c39d35
Michael Vo <zax@zaxu.xyz>
2022-07-01 19:04:04 +1000

    style: autoformat src

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,5 +1,5 @@
-import tw, { globalStyles } from "twin.macro";
-import { globalCss } from "../../stitches.config";
+import tw, { globalStyles } from 'twin.macro';
+import { globalCss } from '../../stitches.config';
 
 const customStyles = {
   body: tw`antialiased`,
