1160cefc2fc9ca8e4783f88943cadcf934237bdb
Michael Vo <zax@zaxu.xyz>
2022-07-01 18:31:46 +1000

    chore(tailwind): remove vue from config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     colors,
     extend: {},
