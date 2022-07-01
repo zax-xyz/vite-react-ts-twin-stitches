4196ef72acf93a90e4839e347b35ed6686c4c2de
Michael Vo <zax@zaxu.xyz>
2022-07-01 19:05:09 +1000

    fix: ignore non-null-assertion for react root creation

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import globalStyles from './styles/globalStyles';
 
 globalStyles();
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
