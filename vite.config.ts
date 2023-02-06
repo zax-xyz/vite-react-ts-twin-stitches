9f41c6bb6c4a5f07e2399e2b9106783a8b6ebf9a
Michael Vo <zax@zaxu.xyz>
2023-02-06 17:40:19 +1100

    chore: add link to vite issue in comment

--- vite.config.ts
+++ vite.config.ts
@@ -14,10 +14,11 @@ export default defineConfig({
   ],
   optimizeDeps: {
     esbuildOptions: {
-      target: "es2020",
+      target: 'es2020',
     },
   },
   esbuild: {
+    // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
     logOverride: { 'this-is-undefined-in-esm': 'silent' },
   },
 });
